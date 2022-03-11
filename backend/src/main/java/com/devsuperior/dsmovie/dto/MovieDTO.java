3190b17571fb512ccacb0329a927e2e4e93b0a25
Junior Vieira <junior.vieira.1990@gmail.com>
2022-03-11 07:41:53 -0300

    Find movies

---
+++ backend/src/main/java/com/devsuperior/dsmovie/dto/MovieDTO.java
@@ -0,0 +1,26 @@
+package com.devsuperior.dsmovie.dto;
+
+import com.devsuperior.dsmovie.entities.Movie;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MovieDTO {
+
+    private Long id;
+    private String title;
+    private Double score;
+    private Integer count;
+    private String image;
+
+    public MovieDTO(Movie movie) {
+        this.id = movie.getId();
+        this.title = movie.getTitle();
+        this.score = movie.getScore();
+        this.count = movie.getCount();
+        this.image = movie.getImage();
+    }
+}
